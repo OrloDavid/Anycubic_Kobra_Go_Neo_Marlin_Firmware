921985788e33e2936eef960000809db4a07342d0
DavidOrlo <david@davidorlo.com>
2023-11-26 09:15:58 -0500

    m11

--- source/Marlin/src/module/thermistor/thermistor_998.h
+++ source/Marlin/src/module/thermistor/thermistor_998.h
@@ -21,33 +21,13 @@
  */
 #pragma once
 
-#include "../inc/MarlinConfigPre.h"
-#include "../gcode/parser.h"
+// User-defined table 1
+// Dummy Thermistor table.. It will ALWAYS read a fixed value.
+#ifndef DUMMY_THERMISTOR_998_VALUE
+  #define DUMMY_THERMISTOR_998_VALUE 25
+#endif
 
-#include <stdint.h>
-
-#define MAX_REPEAT_NESTING 10
-
-typedef struct {
-  uint32_t sdpos;   // The repeat file position
-  int16_t counter;  // The counter for looping
-} repeat_marker_t;
-
-class Repeat {
-private:
-  static repeat_marker_t marker[MAX_REPEAT_NESTING];
-  static uint8_t index;
-public:
-  static inline void reset() { index = 0; }
-  static inline bool is_active() {
-    LOOP_L_N(i, index) if (marker[i].counter) return true;
-    return false;
-  }
-  static bool is_command_M808(char * const cmd) { return cmd[0] == 'M' && cmd[1] == '8' && cmd[2] == '0' && cmd[3] == '8' && !NUMERIC(cmd[4]); }
-  static void early_parse_M808(char * const cmd);
-  static void add_marker(const uint32_t sdpos, const uint16_t count);
-  static void loop();
-  static void cancel();
+const temp_entry_t temptable_998[] PROGMEM = {
+  { OV(   1), DUMMY_THERMISTOR_998_VALUE },
+  { OV(1023), DUMMY_THERMISTOR_998_VALUE }
 };
-
-extern Repeat repeat;
